48bb4340470405fdde512493a68541654dbcf71c
Supanut1911 <ynotmela@gmail.com>
2021-08-02 15:31:31 +0700

    add timestamp to trac every insert log

--- movies-second-service/src/movie-sec/movie-sec.service.ts
+++ movies-second-service/src/movie-sec/movie-sec.service.ts
@@ -1,26 +1,35 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
+import * as moment from 'moment'
 
 let moviesSec = [
     {
       "name": "Avatar",
       "director": "OB1",
-      "style": "SIFI"
+      "style": "SIFI",
+      "createAt": moment('2021-08-01').format(),
+      "updateAt": "" 
     },
     {
       name: "DOOM",
       director: "Jera nemo",
-      style: "Action"
+      style: "Action",
+      "createAt": moment('2021-08-01').format(),
+      "updateAt": "" 
     },
     {
       name: "The1973",
       director: "Sam smith",
-      style: "War"
+      style: "War",
+      "createAt": moment('2021-08-01').format(),
+      "updateAt": "" 
     },
     {
       name: "LionKing",
       director: "Disney conner",
-      style: "Animation"
+      style: "Animation",
+      "createAt": moment('2021-08-01').format(),
+      "updateAt": "" 
     },
 ]
 
@@ -37,19 +46,26 @@ export class MovieSecService {
       data
     ): void {
   
-      let index = moviesSec.findIndex( (e) => {
-        console.log('eee -->', e.name);
-        
+      let index = moviesSec.findIndex( (e) => { 
         return e.name == data.payload.movieName
       })
-        moviesSec[index].director = data.payload.director
+
+      moviesSec[index].director = data.payload.director
+      moviesSec[index].updateAt = moment().format()
+
+    //only insert
+      let newUpdateDirector = {
+        data: data.payload,
+        timeStamp: moment().format()
+      }
+      logMovies.push(newUpdateDirector)
     }
 
     getLogMovieSecDirectorMessage(
 
     ) {
       return {
-        movieLog: logMovies
+        movieSECLog: logMovies
       }
     }
 }
